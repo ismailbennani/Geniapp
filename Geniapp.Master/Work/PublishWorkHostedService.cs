3c195de4a25b94979966fedb1e96c677c31edba4
ismailbennani <ismail.lahkim.bennani@gmail.com>
2024-10-11 23:36:55 +0200

    improve work publication

--- Geniapp.Master/Work/PublishWorkHostedService.cs
+++ Geniapp.Master/Work/PublishWorkHostedService.cs
@@ -1,4 +1,6 @@
-﻿using Geniapp.Infrastructure.Database.MasterDatabase;
+﻿using Geniapp.Infrastructure.Database;
+using Geniapp.Infrastructure.Database.MasterDatabase;
+using Geniapp.Infrastructure.Database.ShardDatabase;
 using Geniapp.Infrastructure.MessageQueue;
 using Geniapp.Infrastructure.Work;
 using Microsoft.EntityFrameworkCore;
@@ -15,6 +17,9 @@ public class PublishWorkHostedService(
     ILogger<PublishWorkHostedService> logger
 ) : BackgroundService
 {
+    readonly DateTime _startTime = DateTime.Now;
+    DateTime _lastTriggerTime = DateTime.MinValue;
+
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
@@ -28,7 +33,7 @@ public class PublishWorkHostedService(
 
             try
             {
-                PublishWork();
+                await PublishWorkAsync();
             }
             catch (Exception exn)
             {
@@ -37,23 +42,43 @@ public class PublishWorkHostedService(
         }
     }
 
-    void PublishWork()
+    async Task PublishWorkAsync()
     {
         using IServiceScope scope = scopeFactory.CreateScope();
-        MasterDbContext masterDbContext = scope.ServiceProvider.GetRequiredService<MasterDbContext>();
+        await using MasterDbContext masterDbContext = scope.ServiceProvider.GetRequiredService<MasterDbContext>();
+
+        Shard[] shards = masterDbContext.Shards.AsNoTracking().ToArray();
+        await Task.WhenAll(shards.Select(PublishWorkOfShard));
+
+        _lastTriggerTime = DateTime.Now;
+    }
+
+    async Task PublishWorkOfShard(Shard shard)
+    {
+        using IServiceScope scope = scopeFactory.CreateScope();
+        ShardContextProvider shardContextProvider = scope.ServiceProvider.GetRequiredService<ShardContextProvider>();
         MessageQueueAdapter adapter = scope.ServiceProvider.GetRequiredService<MessageQueueAdapter>();
 
-        int tenantsCount = masterDbContext.TenantShardAssociations.AsNoTracking().Count();
-        int randomTenant = Random.Shared.Next(tenantsCount);
-        TenantShardAssociation? tenantShardAssociation = masterDbContext.TenantShardAssociations.AsNoTracking().OrderBy(t => t.Id).Skip(randomTenant).FirstOrDefault();
-
-        if (tenantShardAssociation == null)
+        ShardDbContext? context = await shardContextProvider.GetShardContextAsync(shard.Name);
+        if (context == null)
         {
-            logger.LogInformation("No tenant found, no work to publish.");
+            logger.LogWarning("Could not create context for shard {Shard}.", shard.Name);
             return;
         }
 
-        WorkItem workItem = new() { TenantId = tenantShardAssociation.TenantId };
-        adapter.PublishWork(workItem);
+        await using ShardDbContext _ = context;
+        Tenant[] tenants = await context.TenantsData.AsNoTracking()
+            .Where(d => d.LastModificationDate < _startTime || d.LastModificationDate > _lastTriggerTime)
+            .Select(d => d.Tenant)
+            .ToArrayAsync();
+        Random.Shared.Shuffle(tenants);
+
+        foreach (Tenant tenant in tenants)
+        {
+            WorkItem workItem = new() { TenantId = tenant.Id };
+            adapter.PublishWork(workItem);
+
+            await Task.Yield();
+        }
     }
 }
